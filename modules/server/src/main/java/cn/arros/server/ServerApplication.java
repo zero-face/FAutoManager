a59e1f0777bf211f79e88abb1567bcfd99301c8d
facecheng <facecheng@tencent.com>
2022-05-26 21:51:41 +0800

    feat(global): 0.2版本新增创建建仓库

--- modules/server/src/main/java/cn/arros/server/ServerApplication.java
+++ modules/server/src/main/java/cn/arros/server/ServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Author Verge
  * @Date 2021/10/30 23:47
  * @Version 1.0
+ * TODO: 新增通过在平台上创建项目自动在github上创建仓库功能
  */
 @SpringBootApplication
 public class ServerApplication {
