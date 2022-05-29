6180bd8fb121143af77fac62ea46d5d8dd765aa1
zero <1444171773@qq.com>
2022-05-29 21:40:06 +0800

    refactor(AutoConfiguration): 优化

--- plugins/arros-core/src/main/java/cn/arros/plugin/core/log/log4j2/LogCollectorAppender.java
+++ plugins/arros-core/src/main/java/cn/arros/plugin/core/log/log4j2/LogCollectorAppender.java
@@ -23,7 +23,6 @@ import java.io.Serializable;
 public class LogCollectorAppender extends AbstractAppender {
     public static final String PLUGIN_NAME = "ArrosLogCollector";
 
-
     protected LogCollectorAppender(String name,
                                 Filter filter,
                                 Layout<? extends Serializable> layout,
