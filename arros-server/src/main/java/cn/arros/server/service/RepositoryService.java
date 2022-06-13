2b03f3a635e11b2a3cf6a49d4890aa20827236f2
zero <1444171773@qq.com>
2022-06-13 20:53:15 +0800

    refactor(global): 重构

--- arros-server/src/main/java/cn/arros/server/service/RepositoryService.java
+++ arros-server/src/main/java/cn/arros/server/service/RepositoryService.java
@@ -1,6 +1,7 @@
 package cn.arros.server.service;
 
 import cn.arros.server.entity.Repository;
+import cn.arros.server.entity.dto.HookConfig;
 import com.baomidou.mybatisplus.extension.service.IService;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
@@ -14,4 +15,10 @@ import org.eclipse.jgit.api.errors.GitAPIException;
  */
 public interface RepositoryService extends IService<Repository> {
     int addRepo(Repository repository) throws GitAPIException;
+
+    boolean createRepo(Repository repository, String token) throws RuntimeException;
+
+    Integer deleteRepo(Repository repository, String token) throws RuntimeException;
+
+//    boolean createWebhook(Repository repository, String token) throws RuntimeException;
 }
